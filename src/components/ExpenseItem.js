6c96a673e82e172eb3cb8a795c51c3b5b5edc456
jean <jbontikous@yahoo.fr>
2021-12-03 21:54:08 +0100

    35

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,3 +1,13 @@
-const ExpenseItem = (props) => <h2>Expense Item</h2>;
+import './ExpenseItem.css';
+
+const ExpenseItem = (props) => (
+  <div className='expense-item'>
+    <div>March 28th 2021</div>
+    <div className='expense-item__description'>
+      <h2>Car Insurance</h2>
+      <div className='expense-item__price'>$294.67</div>
+    </div>
+  </div>
+);
 
 export default ExpenseItem;
