9ac1a744e52eaa3c9d6150b6914afe1d3f9c84ab
jean <jbontikous@yahoo.fr>
2021-12-04 09:17:44 +0100

    37

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,13 @@
 import './ExpenseItem.css';
 
-const ExpenseItem = (props) => {
-  const expenseDate = new Date(2021, 2, 23);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.67;
-
-  return (
-    <div className='expense-item'>
-      <div>{expenseDate.toISOString()}</div>
-      <div className='expense-item__description'>
-        <h2>{expenseTitle}</h2>
-        <div className='expense-item__price'>${expenseAmount}</div>
-      </div>
+const ExpenseItem = (props) => (
+  <div className='expense-item'>
+    <div>{props.date.toISOString()}</div>
+    <div className='expense-item__description'>
+      <h2>{props.title}</h2>
+      <div className='expense-item__price'>${props.amount}</div>
     </div>
-  );
-};
+  </div>
+);
 
 export default ExpenseItem;
