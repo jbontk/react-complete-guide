e114a7de75ee41bc66b300c7a8c206fcc948d533
jean <jbontikous@yahoo.fr>
2021-12-04 17:24:13 +0100

    53

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,25 +1,30 @@
 import './ExpenseForm.css';
 
-const ExpenseForm = () => (
-  <form>
-    <div className='new-expense__controls'>
-      <div className='new-expense__control'>
-        <label>Title</label>
-        <input type='text' />
+const ExpenseForm = () => {
+  const titleChangeHandler = (event) =>
+    console.log('title changed', event.target.value);
+
+  return (
+    <form>
+      <div className='new-expense__controls'>
+        <div className='new-expense__control'>
+          <label>Title</label>
+          <input type='text' onChange={titleChangeHandler} />
+        </div>
+        <div className='new-expense__control'>
+          <label>Amount</label>
+          <input type='number' min='0.01' step='0.01' />
+        </div>
+        <div className='new-expense__control'>
+          <label>Date</label>
+          <input type='date' min='2019-01-01' max='2022-12-31' />
+        </div>
       </div>
-      <div className='new-expense__control'>
-        <label>Amount</label>
-        <input type='number' min='0.01' step='0.01' />
+      <div className='new-expense__actions'>
+        <button>Add Expense</button>
       </div>
-      <div className='new-expense__control'>
-        <label>Date</label>
-        <input type='date' min='2019-01-01' max='2022-12-31' />
-      </div>
-    </div>
-    <div className='new-expense__actions'>
-      <button>Add Expense</button>
-    </div>
-  </form>
-);
+    </form>
+  );
+};
 
 export default ExpenseForm;
