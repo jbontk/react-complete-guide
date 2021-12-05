39ad653140ea2569a07ae938859437e8f6adb3b6
root <root@space>
2021-12-05 17:20:08 +0000

    Assignment 2

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,18 +1,30 @@
+import { useState } from 'react';
+
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
+import ExpensesFilter from "./Expensesfilter";
 
-const Expenses = ({ items }) => (
-  <Card className='expenses'>
-    {items.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ))}
-  </Card>
-);
+const Expenses = ({items}) => {
+    const [selectedYear, setSelectedYear] = useState('2020');
+
+    const yearChangeHandler = (year) => setSelectedYear(year);
+
+    return (
+        <div>
+            <ExpensesFilter selected={selectedYear} onYearChange={yearChangeHandler}/>
+            <Card className="expenses">
+                {items.filter(expense => expense.date.getFullYear() === +selectedYear).map((expense) => (
+                    <ExpenseItem
+                        key={expense.id}
+                        title={expense.title}
+                        amount={expense.amount}
+                        date={expense.date}
+                    />
+                ))}
+            </Card>
+        </div>
+    );
+};
 
 export default Expenses;
