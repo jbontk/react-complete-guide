cacb93f3683286633a12e24415a3c49566dc2fb3
jean <jbontikous@yahoo.fr>
2021-12-07 21:31:41 +0100

    67

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,30 +3,37 @@ import { useState } from 'react';
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
-import ExpensesFilter from "./Expensesfilter";
+import ExpensesFilter from './Expensesfilter';
 
-const Expenses = ({items}) => {
-    const [selectedYear, setSelectedYear] = useState('2021');
+const Expenses = ({ items }) => {
+  const [selectedYear, setSelectedYear] = useState('2021');
 
-    const yearChangeHandler = (year) => setSelectedYear(year);
+  const yearChangeHandler = (year) => setSelectedYear(year);
 
-    const filteredExpenses = items.filter(expense => expense.date.getFullYear().toString() === selectedYear);
+  const filteredExpenses = items.filter(
+    (expense) => expense.date.getFullYear().toString() === selectedYear
+  );
 
-    return (
-        <div>
-            <ExpensesFilter selected={selectedYear} onYearChange={yearChangeHandler}/>
-            <Card className="expenses">
-                {filteredExpenses.map((expense) => (
-                    <ExpenseItem
-                        key={expense.id}
-                        title={expense.title}
-                        amount={expense.amount}
-                        date={expense.date}
-                    />
-                ))}
-            </Card>
-        </div>
-    );
+  let expensesContent = <p>No expenses found.</p>;
+  if (filteredExpenses.length > 0)
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+
+  return (
+    <div>
+      <ExpensesFilter
+        selected={selectedYear}
+        onYearChange={yearChangeHandler}
+      />
+      <Card className='expenses'>{expensesContent}</Card>
+    </div>
+  );
 };
 
 export default Expenses;
