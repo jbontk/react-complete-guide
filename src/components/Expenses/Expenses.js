697b7ad94a6b2ca914a6180ddb98253105cec5d8
jean <jbontikous@yahoo.fr>
2021-12-07 21:23:05 +0100

    Clarity & toString with ===

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,15 +6,17 @@ import Card from '../UI/Card';
 import ExpensesFilter from "./Expensesfilter";
 
 const Expenses = ({items}) => {
-    const [selectedYear, setSelectedYear] = useState('2020');
+    const [selectedYear, setSelectedYear] = useState('2021');
 
     const yearChangeHandler = (year) => setSelectedYear(year);
+
+    const filteredExpenses = items.filter(expense => expense.date.getFullYear().toString() === selectedYear);
 
     return (
         <div>
             <ExpensesFilter selected={selectedYear} onYearChange={yearChangeHandler}/>
             <Card className="expenses">
-                {items.filter(expense => expense.date.getFullYear() === +selectedYear).map((expense) => (
+                {filteredExpenses.map((expense) => (
                     <ExpenseItem
                         key={expense.id}
                         title={expense.title}
