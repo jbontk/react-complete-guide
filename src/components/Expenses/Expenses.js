49a54c965c03a3578f431f7b66bfe8a8d50af555
jean <jbontikous@yahoo.fr>
2021-12-07 21:44:03 +0100

    68

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 
 import './Expenses.css';
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './Expensesfilter';
+import ExpensesList from './ExpensesList';
 
 const Expenses = ({ items }) => {
   const [selectedYear, setSelectedYear] = useState('2021');
@@ -14,24 +14,15 @@ const Expenses = ({ items }) => {
     (expense) => expense.date.getFullYear().toString() === selectedYear
   );
 
-  let expensesContent = <p>No expenses found.</p>;
-  if (filteredExpenses.length > 0)
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-
   return (
     <div>
-      <ExpensesFilter
-        selected={selectedYear}
-        onYearChange={yearChangeHandler}
-      />
-      <Card className='expenses'>{expensesContent}</Card>
+      <Card className='expenses'>
+        <ExpensesFilter
+          selected={selectedYear}
+          onYearChange={yearChangeHandler}
+        />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
     </div>
   );
 };
