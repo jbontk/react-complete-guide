c599df16c2d232de11db6cfa1a364d81772e7a24
root <root@space>
2021-12-10 20:31:55 +0000

    Remove unwanted button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,23 +4,17 @@ import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
-const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => setTitle('Updated!');
-
-  return (
-    <li>
-      <Card className='expense-item'>
-        <ExpenseDate date={props.date} />
-        <div className='expense-item__description'>
-          <h2>{title}</h2>
-          <div className='expense-item__price'>${props.amount}</div>
-        </div>
-        <button onClick={clickHandler}>Change Title</button>
-      </Card>
-    </li>
-  );
-};
+const ExpenseItem = (props) =>
+    (
+        <li>
+          <Card className="expense-item">
+            <ExpenseDate date={props.date}/>
+            <div className="expense-item__description">
+              <h2>{title}</h2>
+              <div className="expense-item__price">${props.amount}</div>
+            </div>
+          </Card>
+        </li>
+    );
 
 export default ExpenseItem;
