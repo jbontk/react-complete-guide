cdb73b79a5d63291e1947cb5ae5eda8b2b61ac64
jean <jbontikous@yahoo.fr>
2021-12-16 21:40:58 +0100

    91: my attempt

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,16 +1,20 @@
+import Card from '../UI/Card';
+
 const AddUser = (props) => {
   const addUserhandler = (event) => {
     event.preventDefault();
   };
 
   return (
-    <form onSubmit={addUserhandler}>
-      <label htmlFor='username'>Username</label>
-      <input id='username' type='text' />
-      <label htmlFor='age'>Age (Years)</label>
-      <input id='age' type='number' />
-      <button>Add User</button>
-    </form>
+    <Card>
+      <form onSubmit={addUserhandler}>
+        <label htmlFor='username'>Username</label>
+        <input id='username' type='text' />
+        <label htmlFor='age'>Age (Years)</label>
+        <input id='age' type='number' />
+        <button>Add User</button>
+      </form>
+    </Card>
   );
 };
 
