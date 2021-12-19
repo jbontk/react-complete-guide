be68b16d2209d2fb8f8814ea010b872e03d42c22
jean <jbontikous@yahoo.fr>
2021-12-19 10:47:49 +0100

    107

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -2,7 +2,7 @@ import classes from './ErrorModal.module.css';
 import Card from './Card';
 import Button from './Button';
 import { Fragment } from 'react';
-import rd from 'react-dom';
+import ReactDom from 'react-dom';
 
 const Backdrop = (props) => (
   <div className={classes.backdrop} onClick={props.onClick} />
@@ -23,11 +23,11 @@ const ModalOverlay = (props) => (
 const ErrorModal = (props) => {
   return (
     <Fragment>
-      {rd.createPortal(
+      {ReactDom.createPortal(
         <Backdrop onClick={props.onClose} />,
         document.getElementById('backdrop-root')
       )}
-      {rd.createPortal(
+      {ReactDom.createPortal(
         <ModalOverlay
           title={props.title}
           message={props.message}
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 import Button from '../UI/Button';
 import Card from '../UI/Card';
@@ -6,16 +6,19 @@ import ErrorModal from '../UI/ErrorModal';
 import styles from './AddUser.module.css';
 
 const AddUser = (props) => {
-  const [enteredUser, setEnteredUser] = useState('');
-  const [enteredAge, setEnteredAge] = useState('');
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
   const [error, setError] = useState();
 
-  const closeErrorModalHandler = () => setError();
+  const closeErrorModalHandler = () => setError(null);
 
   const addUserhandler = (event) => {
     event.preventDefault();
+    const enteredNameRef = nameInputRef?.current?.value;
+    const enteredAgeRef = ageInputRef?.current?.value;
 
-    if (!enteredUser.trim().length || !enteredAge.trim().length) {
+    if (!enteredNameRef.trim().length || !enteredAgeRef.trim().length) {
       setError({
         title: 'Invalid input',
         message: 'Please enter a valid name and age (non-empty values)',
@@ -23,7 +26,7 @@ const AddUser = (props) => {
       return;
     }
 
-    if (+enteredAge < 1) {
+    if (+enteredAgeRef < 1) {
       setError({
         title: 'Invalid input',
         message: 'Please enter a valid age',
@@ -32,17 +35,20 @@ const AddUser = (props) => {
     }
 
     const user = {
-      name: enteredUser,
-      age: enteredAge,
+      name: enteredNameRef,
+      age: enteredAgeRef,
       id: Math.random().toString(),
     };
     props.onAdd(user);
-    setEnteredUser('');
-    setEnteredAge('');
-  };
 
-  const usernameChangedHandler = (event) => setEnteredUser(event.target.value);
-  const ageChangedHandler = (event) => setEnteredAge(event.target.value);
+    // Ref vs. State, when to use? If you just want to read a value, use Ref which is lighter
+
+    // It is not usually recommended to change the DOM (here by changing .current.value)
+    // Here, it is arguably fine because we are not a creating/removing a DOM element, we are just resetting
+    // two inputs elements
+    nameInputRef.current.value = '';
+    ageInputRef.current.value = '';
+  };
 
   return (
     <>
@@ -56,19 +62,9 @@ const AddUser = (props) => {
       <Card className={styles.input}>
         <form onSubmit={addUserhandler}>
           <label htmlFor='username'>Username</label>
-          <input
-            id='username'
-            type='text'
-            value={enteredUser}
-            onChange={usernameChangedHandler}
-          />
+          <input id='username' type='text' ref={nameInputRef} />
           <label htmlFor='age'>Age (Years)</label>
-          <input
-            id='age'
-            type='number'
-            value={enteredAge}
-            onChange={ageChangedHandler}
-          />
+          <input id='age' type='number' ref={ageInputRef} />
           <Button type='submit'>Add User</Button>
         </form>
       </Card>
