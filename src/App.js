e1f437ba7b2363b388b50b519dbe85edb9152417
jean <jbontikous@yahoo.fr>
2022-02-06 17:46:03 +0100

    157: useCallback and its dependencies (variables used inside a function => !! closures !!)

--- src/App.js
+++ src/App.js
@@ -7,20 +7,28 @@ import './App.css';
 
 const App = (props) => {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   console.log('APP RUNNING');
 
   // With useCallback hook, toggleParagraphHandler is guaranteed to be always the same object in memory
   // (useCallback "saves" our function for subsequent calls)
   // the 2nd argument, the array of dependencies is empty because it never changes
-  // (we could have added setShowParagraph as a dependency, but by design of React's useState hook, that setter state function never 
+  // (we could have added setShowParagraph as a dependency, but by design of React's useState hook, that setter state function never
   // changes (neither shallowly nor deeply))
-  const toggleParagraphHandler = useCallback(() => setShowParagraph((prev) => !prev), []);
+  const toggleParagraphHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowParagraph((prev) => !prev);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => setAllowToggle(true);
 
   return (
     <div className='app'>
       <h1>Hi there!</h1>
-      <DemoOutput show={false} />
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow toggling</Button>
       <Button onClick={toggleParagraphHandler}>Toggle paragraph</Button>
     </div>
   );
