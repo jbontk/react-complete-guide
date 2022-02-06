2efa768196aa3e172d791930eda0f978d662604c
jean <jbontikous@yahoo.fr>
2022-02-06 10:20:00 +0100

    154: a closer look at child component re-evaluation (parent re-evaluated => every child component is re-evaluated)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Button from './components/UI/Button/Button';
+import DemoOutput from './components/Demo/DemoOutput';
 
 import './App.css';
 
-function App() {
+const App = (props) => {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  console.log('APP RUNNING');
+
+  const toggleParagraphHandler = () => setShowParagraph((prev) => !prev);
+
   return (
-    <div className="app">
+    <div className='app'>
       <h1>Hi there!</h1>
+      <DemoOutput show={false} />
+      <Button onClick={toggleParagraphHandler}>Toggle paragraph</Button>
     </div>
   );
-}
+};
 
 export default App;
