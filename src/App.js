1746af3fe622b1cc18e025ad7aa94b2cbee36ee2
jean <jbontikous@yahoo.fr>
2022-02-13 09:39:42 +0100

    Initial checkin + ncu

--- src/App.js
+++ src/App.js
@@ -1,48 +1,11 @@
-import React, { useState, useCallback, useMemo } from 'react';
+import Users from './components/Users';
 
-import Button from './components/UI/Button/Button';
-import DemoOutput from './components/Demo/DemoOutput';
-import DemoList from './components/Demo/DemoList';
-
-import './App.css';
-
-const App = (props) => {
-  const [showParagraph, setShowParagraph] = useState(false);
-  const [allowToggle, setAllowToggle] = useState(false);
-  const [listTitle, setListTitle] = useState('My List');
-
-  console.log('APP RUNNING');
-
-  // With useCallback hook, toggleParagraphHandler is guaranteed to be always the same object in memory
-  // (useCallback "saves" our function for subsequent calls)
-  // the 2nd argument, the array of dependencies is empty because it never changes
-  // (we could have added setShowParagraph as a dependency, but by design of React's useState hook, that setter state function never
-  // changes (neither shallowly nor deeply))
-  const toggleParagraphHandler = useCallback(() => {
-    if (allowToggle) {
-      setShowParagraph((prev) => !prev);
-    }
-  }, [allowToggle]);
-
-  const allowToggleHandler = () => setAllowToggle(true);
-
-  
-  const changeTitleHandler = useCallback(() => {
-    setListTitle('New Title');
-  }, []);
-
-  const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
-
+function App() {
   return (
-    <div className='app'>
-      <h1>Hi there!</h1>
-      <DemoList title={listTitle} items={listItems} />
-      <DemoOutput show={showParagraph} />
-      <Button onClick={changeTitleHandler}>Change List Title</Button>
-      <Button onClick={allowToggleHandler}>Allow toggling</Button>
-      <Button onClick={toggleParagraphHandler}>Toggle paragraph</Button>
+    <div>
+      <Users />
     </div>
   );
-};
+}
 
 export default App;
