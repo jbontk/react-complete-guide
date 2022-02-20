acad7a5597f6cb4388f678e9b5d7020021f6ca0c
jean <jbontikous@yahoo.fr>
2022-02-20 18:36:45 +0100

    177: sending a GET Request

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+
+  const fetchMoviesHandler = () =>
+    fetch('https://swapi.py4e.com/api/films')
+      .then((res) => res.json())
+      .then((res) => {
+        const transformedMovies = res.results.map((m) => ({
+          id: m.episode_id,
+          title: m.title,
+          releaseDate: m.release_date,
+          openingText: m.opening_crawl,
+        }));
+        return setMovies(transformedMovies);
+      });
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>
   );
