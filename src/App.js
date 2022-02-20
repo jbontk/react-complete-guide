3fce47202fd4056555c7d5bcd4d024abf80be67b
jean <jbontikous@yahoo.fr>
2022-02-20 18:57:05 +0100

    182: handling http errors

--- src/App.js
+++ src/App.js
@@ -6,32 +6,50 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const fetchMoviesHandler = async () => {
     setIsLoading(true);
-    const response = await fetch('https://swapi.py4e.com/api/films');
-    const data = await response.json();
+    setError(null);
+    try {
+      const response = await fetch('https://swapi.py4e.com/api/films');
+      if (!response.ok) throw new Error('Something went wrong');
 
-    const transformedMovies = data.results.map((m) => ({
-      id: m.episode_id,
-      title: m.title,
-      releaseDate: m.release_date,
-      openingText: m.opening_crawl,
-    }));
-    setMovies(transformedMovies);
-    setIsLoading(false);
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((m) => ({
+        id: m.episode_id,
+        title: m.title,
+        releaseDate: m.release_date,
+        openingText: m.opening_crawl,
+      }));
+      setMovies(transformedMovies);
+    } catch (e) {
+      setError(e.message);
+    } finally {
+      setIsLoading(false);
+    }
   };
+
+  let content = <p>Found no movies.</p>;
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
 
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>No Movies to display.</p>}
-        {isLoading && <p>Loading...</p>}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
