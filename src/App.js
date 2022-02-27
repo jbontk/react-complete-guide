7568283a541a79a4d37282add2a980ed7a158a0c
jean <jbontikous@yahoo.fr>
2022-02-27 11:09:25 +0100

    182: Preparing the project for the next steps: POSTing using Firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
@@ -8,42 +9,42 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  // Wrapping inside useCallback to ensure the function is not re-created unnecessarily
-  // , therefore avoiding an infinite loop with the useEffect that comes next which uses
-  // it as a dependency.
-  const fetchMoviesHandler = useCallback(async () =>  {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.py4e.com/api/films'); // original: https://swapi.dev/api/films => but the SSL certificate is invalid
-      if (!response.ok) throw new Error('Something went wrong');
+      const response = await fetch('https://react-http-9cf69-default-rtdb.europe-west1.firebasedatabase.app/movies.json');
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
 
       const data = await response.json();
 
-      const transformedMovies = data.results.map((m) => ({
-        id: m.episode_id,
-        title: m.title,
-        releaseDate: m.release_date,
-        openingText: m.opening_crawl,
-      }));
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
       setMovies(transformedMovies);
-    } catch (e) {
-      setError(e.message);
-    } finally {
-      setIsLoading(false);
+    } catch (error) {
+      setError(error.message);
     }
+    setIsLoading(false);
   }, []);
 
-  // Need to use fetchMoviesHandler as a dependency (rather than [] which would only call it once),
-  // because that handler could depend on external state (e.g. an argument), which could change
-  // hence requiring to re-evaluate that function again.
-  // 
-  // useEffect needs to be moved after the handler, because fetchMoviesHandler is not declared as a
-  // function therefore hoisting won't work.
-  useEffect(() => fetchMoviesHandler(), [fetchMoviesHandler]);
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
 
   let content = <p>Found no movies.</p>;
+
   if (movies.length > 0) {
     content = <MoviesList movies={movies} />;
   }
@@ -58,6 +59,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
