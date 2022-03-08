c3560542f517768c564d272fa25ebd31184cd8fe
jean <jbontikous@yahoo.fr>
2022-03-08 09:08:13 +0100

    188: creating a custom React Hook Function

--- src/App.js
+++ src/App.js
@@ -1,78 +1,12 @@
-import React, { useState, useEffect, useCallback } from 'react';
-
-import MoviesList from './components/MoviesList';
-import AddMovie from './components/AddMovie';
-import './App.css';
-import { MOVIES_API } from './Constants';
+import React from 'react';
+import BackwardCounter from './components/BackwardCounter';
+import ForwardCounter from './components/ForwardCounter';
 
 function App() {
-  const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  const fetchMoviesHandler = useCallback(async () => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(MOVIES_API);
-      if (!response.ok) {
-        throw new Error('Something went wrong!');
-      }
-
-      const data = await response.json();
-
-      const transformedMovies = Object.keys(data).map(k => {
-        return {
-          id: k,
-          title: data[k].title,
-          openingText: data[k].openingText,
-          releaseDate: data[k].releaseDate,
-        };
-      });
-      setMovies(transformedMovies);
-    } catch (error) {
-      setError(error.message);
-    }
-    setIsLoading(false);
-  }, []);
-
-  useEffect(() => {
-    fetchMoviesHandler();
-  }, [fetchMoviesHandler]);
-
-  function addMovieHandler(movie) {
-    fetch(MOVIES_API, {
-      method: 'POST',
-      body: JSON.stringify(movie),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-  }
-
-  let content = <p>Found no movies.</p>;
-
-  if (movies.length > 0) {
-    content = <MoviesList movies={movies} />;
-  }
-
-  if (error) {
-    content = <p>{error}</p>;
-  }
-
-  if (isLoading) {
-    content = <p>Loading...</p>;
-  }
-
   return (
     <React.Fragment>
-      <section>
-        <AddMovie onAddMovie={addMovieHandler} />
-      </section>
-      <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
-      </section>
-      <section>{content}</section>
+      <ForwardCounter />
+      <BackwardCounter />
     </React.Fragment>
   );
 }
--- src/Constants.js
+++
@@ -1,4 +0,0 @@
-const API = 'https://react-http-9cf69-default-rtdb.europe-west1.firebasedatabase.app';
-const MOVIES_API = `${API}/movies.json`;
-
-export {MOVIES_API};
