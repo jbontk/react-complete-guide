8198908470107bbc6dc18cc37fe895cf6325f6d2
jean <jbontikous@yahoo.fr>
2022-03-09 09:35:02 +0100

    189 + 190: using + configuring custom hooks

---
+++ src/hooks/use-counter.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+
+const useCounter = (increment = 1) => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter((prevCounter) => prevCounter + increment);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [increment]);
+
+  return counter;
+};
+
+export default useCounter;
