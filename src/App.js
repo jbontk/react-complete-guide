5ec1ecb65fa28ebf1a8e1c75706410b0976cf9af
jean <jbontikous@yahoo.fr>
2022-03-27 13:38:27 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,10 @@
-import React, { useEffect, useState } from 'react';
-
-import Tasks from './components/Tasks/Tasks';
-import NewTask from './components/NewTask/NewTask';
-import useHttp from './hooks/use-http';
-import { TASKS_API } from './constants';
+import SimpleInput from './components/SimpleInput';
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
-
-  const transformResponseFn = (tasksObj) =>
-    setTasks(
-      Object.keys(tasksObj).map((k) => ({
-        id: k,
-        text: tasksObj[k].text,
-      }))
-    );
-
-  useEffect(() => {
-    fetchTasks({ url: TASKS_API }, transformResponseFn);
-  }, [fetchTasks]);
-
-  const taskAddHandler = (task) => {
-    setTasks((prevTasks) => prevTasks.concat(task));
-  };
-
   return (
-    <React.StrictMode>
-      <React.Fragment>
-        <NewTask onAddTask={taskAddHandler} />
-        <Tasks
-          items={tasks}
-          loading={isLoading}
-          error={error}
-          onFetch={() => fetchTasks({ url: TASKS_API }, transformResponseFn)}
-        />
-      </React.Fragment>
-    </React.StrictMode>
+    <div className="app">
+      <SimpleInput />
+    </div>
   );
 }
 
--- src/constants.js
+++
@@ -1,4 +0,0 @@
-const API = 'https://react-http-9cf69-default-rtdb.europe-west1.firebasedatabase.app';
-const TASKS_API = `${API}/tasks.json`;
-
-export { API, TASKS_API };
