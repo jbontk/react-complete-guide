475c5436c3da93455b13c9d6ffba7200c31e0ab8
Jean Bontikous <jbontikous@pictet.com>
2022-06-03 16:52:39 +0200

    239: Working with Multiple State Properties

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -8,15 +8,16 @@ const Counter = () => {
   const decrementHandler = () => dispatch({type: 'decrement'});
 
   const counter = useSelector(state => state.counter);
+  const showCounter = useSelector(state => state.showCounter);
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => dispatch({type: 'toggle'});
 
   console.log('re-render');
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={increaseBy5Handler}>Increment by 5</button>
