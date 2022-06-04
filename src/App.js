08eb6090f05e91ec433da07221e8ca3c735e607b
jbontk <jbontik@gmail.com>
2022-06-04 19:30:14 +0000

    260: Using an Action Creator Thunk

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import {useDispatch, useSelector} from 'react-redux';
 import {useEffect} from 'react';
-import {CART_API} from './utils/constants';
-import {uiActions} from './store/ui-slice';
 import Notification from './components/UI/Notification';
+import {sendCartData} from './store/cart-slice';
 
 let isInitial = true;
 
@@ -17,41 +16,12 @@ function App() {
   const cart = useSelector(({cart}) => cart);
 
   useEffect(() => {
-    const sendCartData = async () => {
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
 
-      if (isInitial) {
-        isInitial = false;
-        return;
-      }
-
-      dispatch(uiActions.showNotification({
-        status: 'sending',
-        title: 'Sending Cart Data',
-        message: 'Sending Cart Data'
-      }));
-
-      const response = await fetch(CART_API, {method: 'PUT', body: JSON.stringify(cart.items)});
-
-      if (!response.ok) {
-        throw new Error('Failed sending Cart data');
-      }
-      dispatch(uiActions.showNotification({
-        status: 'success',
-        title: 'Sent Cart Data',
-        message: 'Cart Data sent successfully'
-      }));
-    };
-
-    sendCartData().catch(e => {
-
-      console.warn(e);
-
-      return dispatch(uiActions.showNotification({
-        status: 'error',
-        title: 'Failed to send Cart Data',
-        message: `Error while sending Cart Data`
-      }));
-    });
+    dispatch(sendCartData(cart));
 
   }, [cart, dispatch]);
 
