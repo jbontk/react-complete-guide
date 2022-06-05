a6c095d01de6298159d968d96cd8d9e8f9968e69
jbontk <jbontik@gmail.com>
2022-06-05 15:23:58 +0000

    284: Implementing Programmatic (Imperative) Navigation

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,12 +1,17 @@
 import QuoteForm from '../components/quotes/QuoteForm';
 import {useDispatch} from 'react-redux';
 import {quotesActions} from '../store/quotes-slice';
+import {useHistory} from 'react-router-dom';
 
 const NewQuote = () => {
+
+  const history = useHistory();
 
   const dispatch = useDispatch();
   const addQuoteHandler = ({text, author}) => {
     dispatch(quotesActions.addQuote({text, author}));
+
+    history.push('/quotes');
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler}/>;
