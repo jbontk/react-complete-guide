53295b3417c1609848e8e1e833a24dc7e80a9347
jbontk <jbontik@gmail.com>
2022-06-05 15:47:22 +0000

    286: Working with Query Parameters

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,13 +1,33 @@
-import { Fragment } from 'react';
+import {Fragment} from 'react';
 
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
+import {useHistory, useLocation} from 'react-router-dom';
 
 const QuoteList = (props) => {
+
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const isAscending = queryParams.get('sort') === 'asc';
+  const nextSort = isAscending ? 'desc' : 'asc';
+
+  const history = useHistory();
+  const changeSortingHandler = () => history.push(`/quotes?sort=${nextSort}`);
+
   return (
-    <Fragment>
+    <>
+      <div className={classes.sorting}>
+        <button onClick={changeSortingHandler}>Sort by Author: {isAscending ? 'Descending' : 'Ascending'}</button>
+      </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {props.quotes.sort((q1, q2) => {
+            if (isAscending) {
+              return q1.author < q2.author ? -1 : 1;
+            } else {
+              return q1.author < q2.author ? 1 : -1;
+            }
+          }
+        ).map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
@@ -16,7 +36,7 @@ const QuoteList = (props) => {
           />
         ))}
       </ul>
-    </Fragment>
+    </>
   );
 };
 
