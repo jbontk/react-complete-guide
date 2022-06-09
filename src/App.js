3661ba7b73dfade1fb21d004ff7d07ebc374600a
Jean Bontikous <jbontikous@pictet.com>
2022-06-09 12:01:52 +0200

    295: Adding Lazy Loading

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import {Navigate, Route, Routes} from 'react-router-dom';
-import AllQuotes from './pages/AllQuotes';
-import QuoteDetail from './pages/QuoteDetail';
-import NewQuote from './pages/NewQuote';
 import Layout from './components/layout/Layout';
-import NotFound from './pages/NotFound';
+import React, {Suspense} from 'react';
+import LoadingSpinner from './components/UI/LoadingSpinner';
+
+const AllQuotes = React.lazy(() => import('./pages/AllQuotes'));
+const NewQuote = React.lazy(() => import('./pages/NewQuote'));
+const QuoteDetail = React.lazy(() => import('./pages/QuoteDetail'));
+const NotFound = React.lazy(() => import('./pages/NotFound'));
 
 function App() {
   return (
     <Layout>
-      <Routes>
-        <Route path='/' element={<Navigate to='/quotes'/>} />
-        <Route path='/quotes' element={<AllQuotes/>} />
-        <Route path='/quotes/:quoteId/*' element={<QuoteDetail/>} />
-        <Route path='/new-quote' element={<NewQuote/>} />
-        <Route path='*' element={<NotFound/>} /> 
+      <Suspense fallback={<div className='centered'><LoadingSpinner /></div>}>
+        <Routes>
+        <Route path='/' element={<Navigate to='/quotes'/>}/>
+        <Route path='/quotes' element={<AllQuotes/>}/>
+        <Route path='/quotes/:quoteId/*' element={<QuoteDetail/>}/>
+        <Route path='/new-quote' element={<NewQuote/>}/>
+        <Route path='*' element={<NotFound/>}/>
       </Routes>
+      </Suspense>
     </Layout>);
 }
 
