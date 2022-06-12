f76432a0fca7e19100ad4b316948545dc4ecea7e
jean <jbontikous@yahoo.fr>
2022-06-12 16:57:18 +0200

    343: Sending Http Requests to Our API Routes - fin

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,11 +1,17 @@
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 import axios from 'axios';
+import {useRouter} from 'next/router';
 
 function NewMeetupPage() {
+
+  const navigate = useRouter();
+
   const addMeetupHandler = async (data) => {
     const response = await axios.post('/api/new-meetup', data, {headers: {'content-type': 'application/json'}});
 
     console.log(response);
+
+    navigate.push('/');
   };
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler}/>;
