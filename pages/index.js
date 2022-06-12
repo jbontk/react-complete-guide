9e70250a1a8aa67f6b19cf58da3098a553b23e69
jbontk <jbontik@gmail.com>
2022-06-12 13:21:09 +0000

    338: Exploring Server-side Rendering (SSR) with "getServerSideProps"

--- pages/index.js
+++ pages/index.js
@@ -19,13 +19,32 @@ function HomePage(props){
   return <MeetupList {...props} />
 }
 
+// export const getServerSideProps = async (context) => {
+//   // e.g. fetch data from an API...
+//   // pre-render on every request (only executed on the server, never on the client)
+//
+//   const {req, res} = context;
+//   // console.log(req, res);
+//
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS
+//     }
+//   }
+// };
+
 export const getStaticProps = async (context) => {
+  // e.g. fetch data from an API...
+  // pre-render at build time then
+  // after each number of seconds specified in revalidate (if any)
+  // only runs server-side and not on the browser
+  console.log('Get Static Props called..');
   return {
     props: {
       meetups: DUMMY_MEETUPS
     },
-    revalidate: 1 // in seconds
+    revalidate: 10 // in seconds
   }
-}
+};
 
 export default HomePage;
