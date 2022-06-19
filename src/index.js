4ee552410151141f5e554c6be8f511958c1c33b9
jbontk <jbontik@gmail.com>
2022-06-19 10:02:17 +0000

    370: Alternative: Using the Context API 371: Toggling Favorites with the Context API

--- src/index.js
+++ src/index.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
-import {Provider} from 'react-redux';
-import {combineReducers, createStore} from 'redux';
 import {BrowserRouter} from 'react-router-dom';
 
 import './index.css';
 import App from './App';
-import productReducer from './store/reducers/products';
+import ProductsProvider from './context/products-context';
 
-const rootReducer = combineReducers({
-  shop: productReducer
-});
-
-const store = createStore(rootReducer);
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
-  <Provider store={store}>
+  <ProductsProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>);
+  </ProductsProvider>);
