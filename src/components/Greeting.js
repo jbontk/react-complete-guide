53ac2df56309e84670a3850e65726e423ae878d2
jbontk <jbontik@gmail.com>
2022-06-24 21:15:25 +0000

    390: Testing User Interaction & State

---
+++ src/components/Greeting.js
@@ -0,0 +1,15 @@
+import {useState} from 'react';
+
+const Greeting = () => {
+
+  const [changedText, setChangedText] = useState(false)
+
+  return <div>
+    <h2>Hello World!</h2>
+    {!changedText && <p>It's good to see you!</p>}
+    {changedText && <p>Changed!</p>}
+    <button onClick={() => setChangedText(true)}>Change Text!</button>
+  </div>;
+};
+
+export default Greeting;
